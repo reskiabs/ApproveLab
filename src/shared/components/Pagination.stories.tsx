805943b2e5d1c8ff3323c36a0cadb89d96bb55c5
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2026-05-06 21:18:53 +0700

    test: added paginataion stories

--- src/shared/components/Pagination.stories.tsx
+++ src/shared/components/Pagination.stories.tsx
@@ -1,29 +1,110 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
-import { Badge } from "./ui/badge/badge";
+import { useState } from "react";
+import { AppPagination } from "./Pagination";
 
 const meta = {
-  title: "Example/Badge",
-  component: Badge,
+  title: "Components/AppPagination",
+  component: AppPagination,
   parameters: {
     layout: "centered",
   },
   tags: ["autodocs"],
-  args: { variant: "outline", children: "Badge" },
-} satisfies Meta<typeof Badge>;
+
+  args: {
+    onPageChange: () => {},
+  },
+
+  argTypes: {
+    page: {
+      control: "number",
+    },
+    totalPages: {
+      control: "number",
+    },
+    siblingCount: {
+      control: "number",
+    },
+    onPageChange: {
+      action: "pageChanged",
+    },
+  },
+} satisfies Meta<typeof AppPagination>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+function PaginationWithState(args: React.ComponentProps<typeof AppPagination>) {
+  const [page, setPage] = useState(args.page);
 
-export const Outline: Story = {
-  args: { variant: "outline" },
+  return (
+    <AppPagination
+      {...args}
+      page={page}
+      onPageChange={(newPage) => {
+        setPage(newPage);
+
+        args.onPageChange?.(newPage);
+      }}
+    />
+  );
+}
+
+export const Default: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 1,
+    totalPages: 5,
+    siblingCount: 1,
+  },
 };
 
-export const Destructive: Story = {
-  args: { variant: "destructive" },
+export const MiddlePage: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 5,
+    totalPages: 10,
+    siblingCount: 1,
+  },
 };
 
-export const Secondary: Story = {
-  args: { variant: "secondary" },
+export const ManyPages: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 10,
+    totalPages: 50,
+    siblingCount: 2,
+  },
+};
+
+export const FirstPageDisabled: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 1,
+    totalPages: 10,
+  },
+};
+
+export const LastPageDisabled: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 10,
+    totalPages: 10,
+  },
+};
+
+export const WithoutEllipsis: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 2,
+    totalPages: 4,
+  },
+};
+
+export const SinglePage: Story = {
+  render: (args) => <PaginationWithState {...args} />,
+  args: {
+    page: 1,
+    totalPages: 1,
+  },
 };
